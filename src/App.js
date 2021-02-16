325372bb1645cf175e5658ee10c11ee334a2f025
david <daviddecastro42@live.co.uk>
2021-02-16 01:56:48 +0000

    finish birthday reminder user usstate and finish css page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data';
+import Card from './Card';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday today</h3>
+        <Card people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
